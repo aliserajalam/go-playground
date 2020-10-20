8e1ed787103a01b2d8efb6cb2127fe1faf505e38
Ali Alam <contact@aliserajalam.com>
2020-10-20 20:30:42 +1100

    Added Go Types

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # go-playground
 
 Learning objectives:
+
+- Function declaration
+- Built in types
+- Composite types
---
+++ go-types.go
@@ -0,0 +1,51 @@
+package main
+
+// Built-in types
+// string - String
+// bool - Boolean
+// int - Integer
+// float32 - 32 bit float
+// float64 - 64 bit float
+
+// Composite types
+// Array
+// Slice
+// Map
+// Channel
+// Structs
+
+// Function convention
+// keyword func - name of function - (parameter type) - return type(s)
+func add(x, y float64) float64 {
+	return x + y
+}
+
+func multiple(a, b string) (string, string) {
+	return a, b
+}
+
+func main() {
+	// --------------------------------
+
+	// Variable assignments
+
+	// var num1 float64 = 5.6
+	// var num2 float64 = 9.5
+
+	// var num3, num4 float64 = 5.6, 9.5
+
+	// num5, num6 := 5.6, 9.5
+
+	// w1, w2 := "Hello", "Go"
+
+	// --------------------------------
+
+	// Type casting
+
+	// var a int = 6
+	// var b float64 = float64(a)
+
+	// x := a // X is of type int
+
+	// --------------------------------
+}
