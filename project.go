1473dd259ec8962efecb39abbe56d685ef6d359f
Ali Alam <contact@aliserajalam.com>
2020-10-20 22:01:40 +1100

    XML parsing

--- project.go
+++ project.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type SiteMapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
 func main() {
 	// Get request
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
+
 	resp.Body.Close()
+
+	var s SiteMapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
+
 }
